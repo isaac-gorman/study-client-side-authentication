b9a2c158d9c69bd673845e610b25a50b462f0ac6
Isaac Gorman <emailme@isaacg.net>
2020-09-04 12:31:32 -0500

    prog

---
+++ prj/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h4>Client Side Authentication</h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ prj/src/notes/overview.js
@@ -0,0 +1,6 @@
+// Module Learning Objective:
+// - Learn how to work with a server that uses tokens for authentication
+
+// Learning Objectives:
+// - 1) Handle authentication with tokens in a react app
+// - 2) Implement protected routes using an authentication token and Redirect
